e2fed213d239c63d3369fab5eebb1050296f0b5d
RusuDDD <91134710+RusuDDD@users.noreply.github.com>
2021-11-09 14:09:19 +0100

    Update Main.java
    Same theme for homework , just with different conditions

--- src/de/telran/Main.java
+++ src/de/telran/Main.java
@@ -4,25 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student student = new Student("Grisha", "Schustrik", 21, 1);
-        Student studentca = new Student("Maria", "Bombacika", 19, 2);
-        Student studentische = new Student("Aliosha", "Trubacist", 20, 3);
-        Student studentic = new Student("Prinparc", "Trasa", 20, 4);
-        Student st = new Student("Dwein", "Kratik", 19, 5);
+        Student gs1 = new Student("Grisha", "Schustrik", "Estonia", 20 ,1 ,150);
+        Student studentca = new Student("Maria", "Bombacika","Russia" , 19, 2,175 );
+        Student studentische = new Student("Aliosha", "Trubacist", "Romania", 20 ,3, 125);
+        Student studentic = new Student("Prinparc", "Trasa", "Moldova" ,20 , 4 ,135);
+        Student st = new Student("Dwein", "Kratik", "Columbia", 19 ,5,145);
 
-        StudentsWithStipendy hama = new StudentsWithStipendy("Masha", "Pupok", 20, 6, 500);
-        StudentsWithStipendy hama1 = new StudentsWithStipendy("Petea", "Pupokin", 21, 7, 700);
-        StudentsWithStipendy hama2 = new StudentsWithStipendy("Misha", "Pupokacia", 22, 8, 400);
-        StudentsWithStipendy hama3 = new StudentsWithStipendy("Tristan", "Pupokinoia", 19, 9, 600);
-        StudentsWithStipendy hama4 = new StudentsWithStipendy("Gari", "Supoka", 18, 10, 550);
+        Student hama = new Student("Masha", "Pupok", "Peru", 20, 6,500);
+        Student hama1 = new Student("Petea", "Pupokin", "Spane", 21,7, 700);
+        Student hama2 = new Student("Misha", "Pupokacia", "Cipru", 22,8, 800);
+        Student hama3 = new Student("Tristan", "Pupokinoia", "Amsterdam", 19, 9,600);
+        Student hama4 = new Student("Gari", "Supoka", "Belgia", 18, 10,590);
 
-        NewStudents bumbum = new NewStudents("Badi", "Trotykii", "America", 20);
-        NewStudents bumbum1 = new NewStudents("Denis", "Trambo", "Japon", 19);
-        NewStudents bumbum2 = new NewStudents("Cristina", "Taras", "France", 21);
-        NewStudents bumbum3 = new NewStudents("Tolea", "Koki", "Greace", 22);
-        NewStudents bumbum4 = new NewStudents("Mark", "Tuleai", "India", 18);
+        Student bumbum = new Student("Badi", "Trotykii", "America", 20,11 ,250);
+        Student bumbum1 = new Student("Denis", "Trambo", "Japon", 19, 12, 255);
+        Student bumbum2 = new Student("Cristina", "Taras", "France", 21 , 13,265);
+        Student bumbum3 = new Student("Tolea", "Koki", "Greece", 22, 14 ,270);
+        Student bumbum4 = new Student("Mark", "Tuleai", "India", 18, 15, 300);
 
-        student.introduce();
+        gs1.introduce();
         studentca.introduce();
         studentische.introduce();
         studentic.introduce();
@@ -30,18 +30,18 @@ public class Main {
 
         System.out.println();
 
-        hama.introducere();
-        hama1.introducere();
-        hama2.introducere();
-        hama3.introducere();
-        hama4.introducere();
+        hama.introduce();
+        hama1.introduce();
+        hama2.introduce();
+        hama3.introduce();
+        hama4.introduce();
 
         System.out.println();
-        bumbum.introdu();
-        bumbum1.introdu();
-        bumbum2.introdu();
-        bumbum3.introdu();
-        bumbum4.introdu();
+        bumbum.introduce();
+        bumbum1.introduce();
+        bumbum2.introduce();
+        bumbum3.introduce();
+        bumbum4.introduce();
 
     }
 }
