1b35bed002decf481c2c9dfd13ecf6cb2782a52b
RusuDDD <91134710+RusuDDD@users.noreply.github.com>
2021-11-09 14:10:27 +0100

    Update Student.java

--- src/de/telran/Student.java
+++ src/de/telran/Student.java
@@ -4,17 +4,26 @@ public class Student {
 
     String name;
     String surname;
+    String country;
     int age;
     int number;
+    int stipendy;
 
-    public Student(String name, String surname, int age, int number) {
+    public Student(String name, String surname, String country, int age, int number, int stipendy) {
         this.name = name;
         this.surname = surname;
+        this.country = country;
         this.age = age;
         this.number = number;
+        this.stipendy = stipendy;
     }
 
-    void introduce() {
-        System.out.println("This is " + name + " " + surname + " " + "and is" + " " + age + " " + "years old" + " " + "with number" + " " + number);
+    public void introduce() {
+        if (stipendy < 200)
+            System.out.println("This is third class and" + " " + name + " " + surname + " " + "and is" + " " + age + " " + "years old" + " " + "with number" + " " + number + " " + "with" + " " + stipendy+" "+country);
+        else if (stipendy > 500)
+            System.out.println("This is first class and" + " " + name + " " + surname + " " + "and is" + " " + age + " " + "years old" + " " + "with number" + " " + number + " " + "with" + " " + stipendy+" "+country);
+        else
+        System.out.println("This is second third and" + " " + name + " " + surname + " " + "and is" + " " + age + " " + "years old" + " " + "with number" + " " + number + " " + "with" + " " + stipendy+" "+country);
     }
 }
